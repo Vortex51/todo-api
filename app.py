a4b66af676e78838772d471cc47309013e3f9504
Justin Brien <13217702@student.uts.edu.au>
2020-03-21 17:45:12 +1100

    finished backend server

--- app.py
+++ app.py
@@ -58,12 +58,51 @@ def add_todo():
 
     return todo_schema.jsonify(new_todo)
 
-#Get All Products
+#Get All Todo's
 @app.route('/todo', methods=['GET'])
 def get_todos():
     all_todos = Todo.query.all()
     result = todos_schema.dump(all_todos)
-    return jsonify(result.data)
+    return jsonify(result)
+
+#Get Single Todo
+@app.route('/todo/<id>', methods=['GET'])
+def get_todo(id):
+    todo = Todo.query.get(id)
+    return todo_schema.jsonify(todo)
+
+#Update Todo
+@app.route('/todo/<id>', methods=['PUT'])
+def update_todo(id):
+    todo = Todo.query.get(id)
+
+    name = request.json['name']
+    desc = request.json['desc']
+    cat = request.json['cat']
+    due = request.json['due']
+    active = request.json['active']
+
+    fdue = datetime.strptime(due, '%Y-%m-%d')
+
+    todo.name = name
+    todo.desc = desc
+    todo.cat = cat
+    todo.due = fdue
+    todo.active = active
+
+    db.session.commit()
+
+    return todo_schema.jsonify(todo)
+
+#Delete Todo
+@app.route('/todo/<id>', methods=['DELETE'])
+def delete_todo(id):
+    todo = Todo.query.get(id)
+    db.session.delete(todo)
+    db.session.commit()
+    
+    return todo_schema.jsonify(todo)
+
 
 #Run Server
 if __name__ == '__main__':
